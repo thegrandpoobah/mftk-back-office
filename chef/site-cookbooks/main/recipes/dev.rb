30c8f79330bdb00b2ce63890dd843b97960b9cd0
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-01-13 22:18:17 -0500

    Add HTTPS support

--- chef/site-cookbooks/main/recipes/dev.rb
+++ chef/site-cookbooks/main/recipes/dev.rb
@@ -1,6 +1,7 @@
-include_recipe 'nodejs::nodejs_from_package'
+include_recipe 'nodejs'
 include_recipe 'postgresql::client'
 include_recipe 'postgresql::server'
+include_recipe 'openssl'
 
 package 'build-essential'
 package 'git'
@@ -22,3 +23,10 @@ magic_shell_environment 'AWS_SECRET_ACCESS_KEY' do
 	value aws_secret['aws_secret_access_key']
 end
 
+openssl_x509 '/home/vagrant/mftk-back-office/app/config/localhost.crt' do
+  common_name 'localhost'
+  org 'MFTK Martial Arts Academy'
+  org_unit ''
+  key_length 4096
+  country 'CA'
+end
