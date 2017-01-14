2557bf7c2f9b0e7cafe20d5344a296e13dbe5d07
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2017-01-13 23:03:33 -0500

    Add LetsEncrypt SSL endpoints for Production

--- chef/site-cookbooks/main/recipes/prod_setup.rb
+++ chef/site-cookbooks/main/recipes/prod_setup.rb
@@ -1,5 +1,30 @@
-include_recipe 'nodejs::nodejs_from_package'
+include_recipe 'nodejs'
 include_recipe 'monit'
 include_recipe 'postgresql::client'
+include_recipe 'openssl'
 
 package 'zip'
+
+openssl_x509 '/srv/www/shared/localhost.crt' do
+  common_name 'localhost'
+  org 'MFTK Martial Arts Academy'
+  org_unit ''
+  key_length 4096
+  country 'CA'
+end
+
+remote_file '/usr/bin/certbot' do
+  source 'https://dl.eff.org/certbot-auto'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+cron 'update ssl certificate' do
+  minute "27" # this should be random
+  hour "11,23"
+  weekday "*"
+
+  command "/usr/bin/certbot renew --quiet --no-self-upgrade --post-hook \"ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/fullchain.pem /srv/www/shared/localhost.crt; ln --force --symbolic /etc/letsencrypt/live/#{node['cert_domain']}/privkey.pem /srv/www/shared/localhost.key; monit restart mftk-back-office\""
+end
